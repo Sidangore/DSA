6bc54b8e23adc99a850223d29a69e4f62c77e8b4
Siddhant Angore <sid.angore@gmail.com>
2023-09-23 11:45:47 +0530

    Big-O

--- Big-O/Big-O-Analysys.cpp
+++ Big-O/Big-O-Analysys.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+int main () {
+    int a = 0, b = 0;
+    int N = 5, M = 5;
+
+    for (int i = 0; i < N; i++) {
+        a = a + 5;
+    }
+
+    for (int i = 0; i < M; i++) {
+        b = b + 10;
+    }
+
+    cout << a << " " << b << endl;
     return 0;
 }
+
+/**
+ * Here the first loop runs for N times 0 -> N-1
+ * And second loop runs for M times 0 -> M-1
+ * 
+ * The calculation time = O(1)
+ * So adding the time complexities = O(N+M+1) = O(M+N)
+*/
